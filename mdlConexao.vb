cbcd76bd51ff1153c76078584fd9c6bdaa8ac2ae
Luciene Oliveira <Luciene Oliveira@CASA2>
2024-07-07 12:57:12 -0300

    Ajustes Gerais. Exlusão do projeto Escola e Vendas Inclusão de icones nos botões

--- mdlConexao.vb
+++ mdlConexao.vb
@@ -1,3 +1,5 @@
+Imports System.Configuration
+Imports System.Data.SqlClient
 Imports System.ServiceProcess
 Module mdlConexao
     Public Servidor = GetNomeSQLServer()
@@ -13,15 +15,7 @@ Module mdlConexao
     Public Function GetConnection() As System.Data.IDbConnection
         strConexao = $"Data Source={Servidor};Initial Catalog={DataBase};User ID={user}; Password={password};Integrated Security=True"
     End Function
-    Public Sub AlterarStringDeConexao()
-        Dim Config = System.Configuration.ConfigurationManager.OpenExeConfiguration(System.Configuration.ConfigurationUserLevel.None)
-        Dim ConnectionStrings = Config.ConnectionStrings
-        For Each ConnectionString As System.Configuration.ConnectionStringSettings In ConnectionStrings.ConnectionStrings
-            ConnectionString.ConnectionString = String.Format($"Provider=Microsoft.Jet.OLEDB.4.0;Data Source={Servidor}\{DataBase}.mdb", Environment.CurrentDirectory)
-        Next
-        Config.Save(System.Configuration.ConfigurationSaveMode.Modified)
-        System.Configuration.ConfigurationManager.RefreshSection("connectionStrings")
-    End Sub
+
     ''' <summary>
     ''' Esta função obtem o nome do servidor SQL Server.
     ''' </summary>
